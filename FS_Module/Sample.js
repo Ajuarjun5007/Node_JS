754c5a88562ac4ed8eea83332212add395687139
malligarjun <malligarjun.p@tandemcrystals.com>
2025-09-14 12:51:08 +0530

    CodeCommited

--- FS_Module/Sample.js
+++ FS_Module/Sample.js
@@ -1,11 +1,25 @@
 const fs = require('fs');
+const http = require('http');
+
 // Async data
-fs.readFile('../HTTP_Module/website.js',function(err,data){
-    if(err){
-        console.error('error',err);
-    }
-    console.log('data Async:',data.toString());
-})
-// Sync Data
-const result = fs.readFileSync('../execute.js');
-console.log('data Sync:', result.toString());
+// fs.readFile('../HTTP_Module/website.js',function(err,data){
+//     if(err){
+//         console.error('error',err);
+//     }
+//     console.log('data Async:',data.toString());
+// })
+// // Sync Data
+// const result = fs.readFileSync('../execute.js');
+// console.log('data Sync:', result.toString());
+
+http.createServer(function(err,res){
+
+    fs.readFile('../Demo.txt',function(err,data){
+        if(err){
+            console.err('err',err)
+        }
+        console.log('Asyncdata',data.toString())
+        res.write(data);
+        res.end();
+    })
+}).listen(8080);
